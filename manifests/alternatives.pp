1ade51bbcc82c7b490af49dae457be6bb1997128
Bill Weiss <bv@billweiss.net>
2012-10-15 21:51:01 -0500

    Paths are important!
    On a fresh Debian install, I used this module and got:
    root@csv5:/etc/puppet/modules# puppet agent --test
    (stuff)
    err: Failed to apply catalog: 'update-alternatives --set editor
        /usr/bin/vim.nox' is not qualified and no path was specified.
        Please qualify the command or specify a path.
    
    With this addition, I get:
    root@csv5:~# puppet agent --test
    (stuff)
    notice: /Stage[main]/Userland/Debian::Alternatives::Set[editor]/Exec[update-alternatives
        --set editor /usr/bin/vim.nox]/returns: executed successfully
    
    My usage is:
        debian::alternatives::set { "editor":
            path => "/usr/bin/vim.nox",
            require => Package['vim-nox']
        }

--- manifests/alternatives.pp
+++ manifests/alternatives.pp
@@ -1,11 +1,13 @@
 define debian::alternatives::add($link, $path, $priority) {
   exec { "update-alternatives --install $link $name $path $priority":
+    path   => "/usr/sbin",
     unless => "grep $path /var/lib/dpkg/alternatives/$name"
   }
 }
 
 define debian::alternatives::set($path) {
   exec { "update-alternatives --set $name $path":
+    path   => "/usr/sbin",
     unless => "/bin/sh -c '[ -L /etc/alternatives/$name ] && [ /etc/alternatives/$name -ef $path ]'"
   }
 }
